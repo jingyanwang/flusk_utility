77cb2719b1dbafce805f5e13873a1f66ef67118b
jingyanwang <jywang.ieee@gmail.com>
2019-04-16 16:37:09 +0400

    Update argsparser.py

--- argsparser.py
+++ argsparser.py
@@ -3,7 +3,7 @@ import argparse
 from envdefault import EnvDefault
 
 def prepare_args():
-	parser = argparse.ArgumentParser(description="pami")
+	parser = argparse.ArgumentParser(description="")
 	parser.add_argument('--activate_detection', action=EnvDefault, envvar='ACTIVATE_DETECTION', type=bool, default=True)
 	parser.add_argument('--host', action=EnvDefault, envvar='HOST', type=str, default='0.0.0.0')
 	parser.add_argument('--port', action=EnvDefault, envvar='PORT', type=int, default=9000)
