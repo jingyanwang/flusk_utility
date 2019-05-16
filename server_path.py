632add0fbf17f93398899328b9dad7fbbf911254
jingyanwang <jywang.ieee@gmail.com>
2019-05-16 16:15:09 +0400

    Update server_path.py

--- server_path.py
+++ server_path.py
@@ -4,54 +4,31 @@ import argsparser
 from flask_restplus import *
 from flask import *
 
-ns = Namespace('', description='pami')
+ns = Namespace('pami', description='')
 args = argsparser.prepare_args()
 
 parser = ns.parser()
 parser.add_argument('text', type=str, location='json')
-parser.add_argument('language', type=str, location='json')
-parser.add_argument('personal_message', type=str, location='json')
-parser.add_argument('name', type=list, location='json')
-parser.add_argument('title', type=list, location='json')
-parser.add_argument('location', type=list, location='json')
-parser.add_argument('time', type=list, location='json')
 
 req_fields = {'text': fields.String(\
-	example = u"see you in dubai at 7 pm"),\
-	'language':fields.String(example = u"en"),\
-	'message_type':fields.String(example = u"personal_message"),\
-	'name':fields.List(fields.String, example = []),\
-	'title':fields.List(fields.String, example = []),\
-	'location':fields.List(fields.String, example = [u"dubai"]),\
-	'time':fields.List(fields.String, example = [u"7 pm"])\
+	example = u"this is an input text")\
 	}
+pami_req = ns.model('pami_req', req_fields)
 
-meeting_attendee = ns.model('meeting_attendee', \
-	{'meeting_attendee': fields.String,\
-	'meeting_attendee_score': fields.Float})
-
-meeting_time = ns.model('meeting_time', \
-	{'meeting_time': fields.String,\
-	'meeting_time_score': fields.Float})
-
-meeting_location = ns.model('meeting_place', \
-	{'meeting_location': fields.String,\
-	'meeting_location_score': fields.Float})
+attribute = ns.model('', \
+	{'entity': fields.String,\
+	'score': fields.Float})
 
 rsp_fields = {\
-	'meeting_attendee': fields.List(\
-	fields.Nested(meeting_attendee)),\
-	'meeting_time': fields.List(\
-	fields.Nested(meeting_time)),\
-	'meeting_location': fields.List(\
-	fields.Nested(meeting_location)),\
-	'running_time':fields.Float
+	'attribute': fields.List(\
+	fields.Nested(attribute)),\
+	'error':fields.String,\
+	'running_time':fields.Float\
 	}
 
-pami_req = ns.model('pami_req', req_fields)
 pami_rsp = ns.model('pami_rsp', rsp_fields)
 
-@ns.route('/pami_meeting_detection')
+@ns.route('/predict')
 class pami(Resource):
 	def __init__(self, *args, **kwargs):
 		super(pami, self).__init__(*args, **kwargs)
@@ -59,9 +36,11 @@ class pami(Resource):
 	@ns.expect(pami_req)
 	def post(self):
 		try:
-			args = parser.parse_args()
 			start = time.time()
+			args = parser.parse_args()			
 			output = {}
+			output['attribute'] = [{'entity':args['text'],\
+						     'score':0.978}]
 			output['running_time'] = float(time.time()- start)
 			output['error'] = 'success'
 			return output, 200
