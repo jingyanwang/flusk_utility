7ff57896ce14ffbe454c242007ee5ac82db9d97c
Jingyan Wang <jimjywang@gmail.com>
2022-11-18 06:31:44 -0800

    Add files via upload

--- server_path.py
+++ server_path.py
@@ -2,20 +2,22 @@
 import time
 import logging
 import argsparser
-from flask_restplus import *
+from flask_restx import *
 from flask import *
 
-ns = Namespace('jingyanwang_api', description='')
+ns = Namespace('crm_suggestions', description='Jimmy as a service')
 args = argsparser.prepare_args()
 
 parser = ns.parser()
 parser.add_argument('text', type=str, location='json')
 
-req_fields = {'text': fields.String(\
+req_fields = {
+	'text': fields.String(\
 	example = u"this is an input text")\
 	}
-jingyanwang_api_req = ns.model('jingyanwang_api_req', req_fields)
+jingyanwang_api_req = ns.model('crm_suggestions', req_fields)
 
+'''
 attribute = ns.model('', \
 	{'entity': fields.String,\
 	'score': fields.Float})
@@ -28,26 +30,29 @@ rsp_fields = {\
 	}
 
 jingyanwang_api_rsp = ns.model('jingyanwang_api_rsp', rsp_fields)
+'''
 
-@ns.route('/predict')
+@ns.route('/chunking')
 class jingyanwang_api(Resource):
 	def __init__(self, *args, **kwargs):
 		super(jingyanwang_api, self).__init__(*args, **kwargs)
-	@ns.marshal_with(jingyanwang_api_rsp)
+	#@ns.marshal_with(jingyanwang_api_rsp)
 	@ns.expect(jingyanwang_api_req)
 	def post(self):		
 		start = time.time()
 		try:			
 			args = parser.parse_args()		
 			output = {}
-			output['attribute'] = [{'entity':args['text'],\
-						     'score':0.978}]
-			output['error'] = 'success'
+			output['chunks'] = [
+				'chunck1',  
+				'chunck2',
+			]
+			output['status'] = 'success'
 			output['running_time'] = float(time.time()- start)
 			return output, 200
 		except Exception as e:
 			output = {}
-			output['error'] = str(e)
+			output['status'] = str(e)
 			output['running_time'] = float(time.time()- start)
 			return output
 ##############
